33b83fcd86aa482f97a55df5f9d7b055e468ad28
Moshe Schorr <mosheraphael@gmail.com>
2020-04-13 12:09:43 +0300

    added check if it is a real place

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import streamlit as st
 
 from yishuvname import random_yishuv_name
 
+with open('yishuvim.txt') as f:
+    yishuvim = f.readlines()
+yishuvim = [y.strip() for y in yishuvim]
+
 st.title('Yishuv Name Generator')
 
 if st.button('Generate a name'):
-    st.header(random_yishuv_name())
+    name = random_yishuv_name()
+    st.header(name)
+    if name in yishuvim:
+        st.subheader('(This randomly generated name is actually a real place)')
